9b54e443747e15038e1d2a7b859f890e3bd3994f
Shuya68 <s.murakami.a.c.t@gmail.com>
2020-12-05 18:18:52 +0900

    Revert " This reverts commit bfe150ebb992da8232c679f757daf7a3fc7b749a.

--- index.js
+++ index.js
@@ -6,11 +6,4 @@ function add(numbers) {
   }
   return result;
 }
-function multi(numbers) {
-  let result = 1;
-  for (let num of numbers) {
-    result = result * num;
-  }
-  return result;
-}
-module.exports = { add, multi };
+module.exports = { add };
